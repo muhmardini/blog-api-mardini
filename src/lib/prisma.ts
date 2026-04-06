e1d14f59a2d4be135b101d7f2a1eead5b95faa94
muhammad k mardini <mardinimuh@gmail.com>
2026-04-06 17:07:56 +0300

    singleton prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,23 @@
-import { PrismaClient } from '#/generated/prisma/client';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import {env} from '#/'
+    import { PrismaClient } from '#/generated/prisma/client';
+    import { PrismaNeon } from '@prisma/adapter-neon';
+    import {env} from '#/shared/env.ts'
+
+    const createPrismaClient = (): PrismaClient => {
+        const adapter = new PrismaNeon({
+            connectionString: env.DATABASE_URL,
+        })
+        return new PrismaClient({
+            adapter,
+            log: env.NODE_ENV === "development"? ['query', 'error', 'warn'] : ['error'],
+        });
+    };
+
+    const globalForPrisma = globalThis as unknown as {
+        prisma: PrismaClient | undefined;
+    };
+
+    export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+    if(env.NODE_ENV !== "production"){
+        globalForPrisma.prisma = prisma;
+    }
